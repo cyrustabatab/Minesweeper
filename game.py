4b7758660a244c51bb53ca30bbf5bbc7529eee38
Cyrus Tabatabai-Yazdi <24766642+cyrustabatab@users.noreply.github.com>
2020-12-16 22:43:28 -0800

    added uncovering logic and numbers

--- game.py
+++ game.py
@@ -3,13 +3,17 @@ import random
 
 pygame.init()
 
-from tile import Tile
 # 8 by 8, 16 by 16 -> 16 by 30
 # 32 pixel gap at top
 # 32 + 16 * 8
 
+# TODO
+# make it so that they can never die on first turn(switch mine with another)?
+# work on game over code(right now just print game over)
+
 screen_width , screen_height =1500, 800
 screen = pygame.display.set_mode((screen_width,screen_height))
+from tile import Tile
 pygame.display.set_caption("Minesweeper")
 clock = pygame.time.Clock()
 BLACK = (0,0,0)
@@ -22,11 +26,29 @@ board_top_gap = tile_size * 4
 easy_rows = easy_cols = 8
 medium_rows = medium_cols = 16
 hard_rows,hard_cols = 16,30
+easy_mines,medium_mines,hard_mines = 10,40,99
+
+
+def count_neighbor_mines(board,row,col):
+
+    count =0
+    for row_diff in (-1,0,1):
+        for col_diff in (-1,0,1):
+            if row_diff ==0 and col_diff == 0:
+                continue
+            
+            try:
+                if board[row + row_diff][col + col_diff].is_mine:
+                    count += 1
+            except:
+                pass
+
+    return count
 
 def create_board(rows,cols,num_mines):
 
 
-    board = [[Tile(col*tile_size,board_top_gap + row * tile_size,tile_size) for col in range(cols)] for row in range(rows)]
+    board = [[Tile(col*tile_size,board_top_gap + row * tile_size,row,col,tile_size) for col in range(cols)] for row in range(rows)]
     
     
     rows_cols = [(row,col) for row in range(rows) for col in range(cols)]
@@ -49,6 +71,10 @@ def create_board(rows,cols,num_mines):
     tiles = pygame.sprite.Group()
     for row in range(rows):
         for col in range(cols):
+            if not board[row][col].is_mine:
+                mines = count_neighbor_mines(board,row,col)
+                if mines != 0:
+                    board[row][col].set_number(mines)
             tiles.add(board[row][col])
     
     
@@ -58,9 +84,42 @@ def create_board(rows,cols,num_mines):
 
 
 
+def uncover_board(board,tile):
+    
+
+    def uncover_helper(board,row,col,visited):
+        
+        visited.add((row,col))
+
+        for row_diff in (-1,0,1):
+            for col_diff in (-1,0,1):
+                if row_diff == 0 and col_diff == 0:
+                    continue
+                
+                neighbor_row,neighbor_col = row + row_diff,col + col_diff
+                if (neighbor_row,neighbor_col) not in visited:
+                    if 0 <= neighbor_row < len(board) and 0 <= neighbor_col < len(board[0]):
+
+                        tile = board[neighbor_row][neighbor_col]
+                        tile.uncover()
+                        if tile.neighboring_mines == 0:
+                            uncover_helper(board,neighbor_row,neighbor_col,visited)
 
 
-def game(screen_width,screen_height,rows,cols,mines=10):
+
+
+                    
+
+
+    
+
+    visited = set()
+    row,col = tile.row,tile.col
+    uncover_helper(board,row,col,visited)
+
+
+
+def game(screen_width,screen_height,rows,cols,mines):
 
     screen = pygame.display.set_mode((screen_width,screen_height))
 
@@ -75,8 +134,16 @@ def game(screen_width,screen_height,rows,cols,mines=10):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 point = pygame.mouse.get_pos()
                 for tile in tiles:
-                    if tile.clicked_on(point):
-                        break
+                    if event.button == 1:
+                        if tile.clicked_on(point):
+                            if tile.is_mine:
+                                print("GAME OVER")
+                            elif tile.neighboring_mines == 0:
+                                uncover_board(board,tile)
+                            break
+                    elif event.button == 3:
+                        if tile.set_flag(point):
+                            break
 
 
 
@@ -140,11 +207,11 @@ def menu():
                 
 
                 if easy_text_rect.collidepoint(coordinate):
-                    game(tile_size * easy_cols,tile_size * easy_rows + board_top_gap,easy_rows,easy_cols)
+                    game(tile_size * easy_cols,tile_size * easy_rows + board_top_gap,easy_rows,easy_cols,easy_mines)
                 elif medium_text_rect.collidepoint(coordinate):
-                    game(tile_size * medium_cols,tile_size * medium_rows + board_top_gap,medium_rows,medium_cols)
+                    game(tile_size * medium_cols,tile_size * medium_rows + board_top_gap,medium_rows,medium_cols,medium_mines)
                 elif hard_text_rect.collidepoint(coordinate):
-                    game(tile_size * hard_cols,tile_size * hard_rows + board_top_gap,hard_rows,hard_cols)
+                    game(tile_size * hard_cols,tile_size * hard_rows + board_top_gap,hard_rows,hard_cols,hard_mines)
 
                 screen = pygame.display.set_mode((menu_width,menu_height))
 
--- tile.py
+++ tile.py
@@ -1,6 +1,20 @@
 import pygame,os
 
 
+def get_mapping_number_to_image():
+    
+    mapping = {}
+    for number in range(1,9):
+
+        mapping[number] = pygame.image.load(os.path.join('assets',f"{number}.png")).convert_alpha()
+
+    return mapping
+
+
+
+
+
+
 
 
 
@@ -9,38 +23,71 @@ class Tile(pygame.sprite.Sprite):
     block_image = pygame.image.load(os.path.join('assets','block.png'))
     mine_image = pygame.image.load(os.path.join('assets','mine.png'))
     empty_image = pygame.image.load(os.path.join('assets','empty.png'))
+    flag_image =pygame.image.load(os.path.join('assets','flagged.png'))
+
+    number_to_image = get_mapping_number_to_image()
 
 
-    def __init__(self,x,y,tile_size=16):
+    def __init__(self,x,y,row,col,tile_size=16):
         super().__init__()
         self.x = x
         self.y = y
+        self.row = row
+        self.col = col
         self.tile_size = tile_size
         self.hidden_image = self.empty_image
         if tile_size != 16:
-            self.image = pygame.transform.scale(self.block_image,(tile_size,tile_size))
+            self.block_image = pygame.transform.scale(self.block_image,(tile_size,tile_size))
             self.hidden_image = pygame.transform.scale(self.hidden_image,(tile_size,tile_size))
-            
+            self.flag_image = pygame.transform.scale(self.flag_image,(tile_size,tile_size))
+        
+        self.image = self.block_image
 
         self.rect = self.image.get_rect(topleft=(self.x,self.y))
         self.is_mine = False
         self.clicked = False
+        self.flagged = False
+        self.neighboring_mines = 0
 
 
     def mark_mine(self):
         self.is_mine = True
         self.hidden_image = self.mine_image
         if self.tile_size != 16:
-            self.hidden_image = pygame.transform.scale(self.mine_image,(self.tile_size,self.tile_size))
+            self.hidden_image = pygame.transform.scale(self.hidden_image,(self.tile_size,self.tile_size))
+    
 
+    def set_number(self,number):
+
+        self.neighboring_mines = number
+        self.hidden_image = self.number_to_image[number]
+        if self.tile_size != 16:
+            self.hidden_image = pygame.transform.scale(self.hidden_image,(self.tile_size,self.tile_size))
 
     def clicked_on(self,point):
-        
-        if not self.rect.collidepoint(point):
+        if self.flagged or self.clicked or not self.rect.collidepoint(point):
             return False
         self.clicked = True
         self.image = self.hidden_image
         return True
+    
+    def uncover(self):
+        self.clicked = True
+        self.image = self.hidden_image
+
+    def set_flag(self,point):
+        if self.clicked or not self.rect.collidepoint(point):
+            return False
+
+        if self.flagged == True:
+            self.image = self.block_image
+            self.flagged = False
+        else:
+            self.flagged = True
+            self.image = self.flag_image
+        return True
+
+
 
 
 
